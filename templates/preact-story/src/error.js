064705e49458b651ea87dfbf04443210f091a457
Nathan Hoad <nathan@nathanhoad.net>
2017-08-02 13:40:13 +1000

    Fixed the over-zealous template variable substitution

--- templates/preact-story/src/error.js
+++ templates/preact-story/src/error.js
@@ -7,19 +7,21 @@ class Error extends Preact.Component {
     }
     
     render() {
+        const error_style = {
+            background: '#900',
+            color: 'white',
+            padding: '20px',
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            width: '100%',
+            boxShadow: '0 4px 10px rgba(0,0,0,0.4)',
+            zIndex: 1000
+        };
+        
         return (
             <div
-                style={{
-                    background: '#900',
-                    color: 'white',
-                    padding: '20px',
-                    position: 'fixed',
-                    top: '0',
-                    left: '0',
-                    width: '100%',
-                    boxShadow: '0 4px 10px rgba(0,0,0,0.4)',
-                    zIndex: 1000
-                }}>
+                style={error_style}>
                 <pre>
                     {this.props.error.stack}
                 </pre>
