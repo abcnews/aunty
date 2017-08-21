c7fc2afb4a82169e892ec71587e6ea653e1cf100
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-08-21 16:06:22 +1000

    React app worm was missing its className

--- templates/react-app/src/components/App.js
+++ templates/react-app/src/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div className={styles.root}>
-        <img class={styles.worm} src={worm} />
+        <img className={styles.worm} src={worm} />
         <h1>{{projectName}}</h1>
       </div>
     );
