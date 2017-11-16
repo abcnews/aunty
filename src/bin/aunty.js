c74175714e468de0c0ca342b4396cf0dae777a65
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-17 09:18:53 +1000

    aunty.js now loads the local version of itself if it can, rather than importing a global/local cli.js.

--- src/bin/aunty.js
+++ src/bin/aunty.js
@@ -1,39 +1,26 @@
 #!/usr/bin/env node
 
-// External
-const resolve = require('resolve');
+if (!require('import-local')(__filename)) {
+  (async () => {
+    const { createErrorLogo } = require('../utils/branding');
+    const { error, log } = require('../utils/logging');
+    const { cli } = require('../cli');
 
-// Ours
-const { name } = require('../../package');
-const { createErrorLogo } = require('../utils/branding');
-const { error, log } = require('../utils/logging');
+    function exit(err) {
+      if (err) {
+        log(createErrorLogo());
+        error(err);
+        process.exit(1);
+      }
 
-function exit(err) {
-  if (err) {
-    log(createErrorLogo());
-    error(err);
-    process.exit(1);
-  }
+      process.exit(0);
+    }
 
-  process.exit(0);
+    process.on('uncaughtException', exit);
+    process.on('unhandledRejection', exit);
+
+    const [err] = await cli(process.argv.slice(2));
+
+    exit(err);
+  })();
 }
-
-process.on('uncaughtException', exit);
-process.on('unhandledRejection', exit);
-
-let cli;
-let isGlobal;
-
-// Always prefer local CLI
-try {
-  cli = require(resolve.sync(`${name}/src/cli`, { basedir: process.cwd() })).cli;
-} catch (err) {
-  isGlobal = true;
-  cli = require('../cli').cli;
-}
-
-(async () => {
-  const [err] = await cli(process.argv.slice(2), isGlobal);
-
-  exit(err);
-})();
