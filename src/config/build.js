a5b3b5d1f3f94a40b49db197c04767e5657878c7
Colin Gourlay <gourlay.colin@abc.net.au>
2020-02-26 09:43:36 +1000

    Prepare build config for project type-based modification

--- src/config/build.js
+++ src/config/build.js
@@ -6,8 +6,11 @@ const { BUILD_DIRECTORY_NAME } = require('../constants');
 const { combine } = require('../utils/structures');
 const { getProjectConfig } = require('./project');
 
+const PROJECT_TYPES_CONFIG = {
+};
+
 module.exports.getBuildConfig = mem(() => {
-  const { build: projectBuildConfig } = getProjectConfig();
+  const { build: projectBuildConfig, type } = getProjectConfig();
 
   return combine(
     {
@@ -20,6 +23,7 @@ module.exports.getBuildConfig = mem(() => {
       useCSSModules: true,
       showDeprecations: false
     },
+    PROJECT_TYPES_CONFIG[type],
     projectBuildConfig
   );
 });
