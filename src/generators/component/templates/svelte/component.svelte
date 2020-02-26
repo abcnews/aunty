e33305c89e125bc8da3aa5d4676cde112d35af6f
Colin Gourlay <gourlay.colin@abc.net.au>
2020-02-26 10:05:34 +1000

    Add svelte component template

---
+++ src/generators/component/templates/svelte/component.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<style>
+  div {
+    border: 1px solid rgb(255, 115, 0);
+    padding: 20px;
+    text-align: center;
+    color: black;
+  }
+</style>
+
+<div>
+  Find me in
+  <strong>src/components/<%= className %>/<%= className %>.svelte</strong>
+</div>
