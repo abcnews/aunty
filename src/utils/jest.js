006e3868a0437e7a175ec7155b70b8e46a5b0faa
Colin Gourlay <gourlay.colin@abc.net.au>
2020-02-26 10:00:30 +1000

    Add svelte testing setup

--- src/utils/jest.js
+++ src/utils/jest.js
@@ -4,6 +4,7 @@ const path = require('path');
 // External
 const importLazy = require('import-lazy')(require);
 const babelJest = importLazy('babel-jest');
+const svelte = importLazy('svelte/compiler');
 
 // Ours
 const { getBabelConfig } = require('../config/babel');
@@ -11,6 +12,8 @@ const { getBabelConfig } = require('../config/babel');
 const MEDIA_RESOURCE_PATTERN = /\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/;
 const STYLE_RESOURCE_PATTERN = /\.s?css$/;
 const STYLE_RESOURCE_REPLACEMENT = 'module.exports = new Proxy({}, { get: (styles, method) => method });';
+const SVELTE_RESOURCE_PATTERN = /\.svelte$/;
+const SVELTE_STYLES_PATTERN = /<style[^>]*>[\S\s]*?<\/style>/g;
 const VUE_RESOURCE_PATTERN = /\.vue$/;
 
 module.exports = {
@@ -20,6 +23,11 @@ module.exports = {
       return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
     } else if (filename.match(STYLE_RESOURCE_PATTERN)) {
       return STYLE_RESOURCE_REPLACEMENT;
+    }
+
+    // Process Svelte files
+    if (filename.match(SVELTE_RESOURCE_PATTERN)) {
+      return processSvelte(src, filename);
     }
 
     const babelConfig = getBabelConfig();
@@ -33,6 +41,24 @@ module.exports = {
     return babelJest.createTransformer(babelConfig).process(src, filename, config, { sourceMaps: false });
   }
 };
+
+function processSvelte(src, filename) {
+  const srcWithoutStyles = src.replace(SVELTE_STYLES_PATTERN, '');
+
+  const { js, css, ast, warnings, vars, stats } = svelte.compile(srcWithoutStyles, {
+    filename,
+    css: false,
+    accessors: true,
+    dev: true,
+    format: 'cjs'
+  });
+  const { code, map } = js;
+
+  return {
+    code,
+    map
+  };
+}
 
 function processVue(src, filename, babelConfig) {
   // `find-babel-config`is a dependency of `vue-jest` which looks for babel
