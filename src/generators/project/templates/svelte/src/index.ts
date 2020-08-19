6ece6651b47211d98c8181e0c6a0a890ad8c4f0e
Colin Gourlay <gourlay.colin@abc.net.au>
2020-08-19 15:59:20 +1000

    Svelte no longer allows a null component target.

--- src/generators/project/templates/svelte/src/index.ts
+++ src/generators/project/templates/svelte/src/index.ts
@@ -3,12 +3,16 @@ import App from './components/App/App.svelte';
 const PROJECT_NAME<% if (isTS) { %>: string<% } %> = '<%= projectSlug %>';
 const ROOT_SELECTOR = `[data-${PROJECT_NAME}-root]`;
 
-new App({
-  target: document.querySelector(ROOT_SELECTOR),
-  props: {
-    projectName: PROJECT_NAME
-  }
-});
+const root = document.querySelector(ROOT_SELECTOR);
+
+if (root) {
+  new App({
+    target: root,
+    props: {
+      projectName: PROJECT_NAME
+    }
+  });
+}
 
 if (process.env.NODE_ENV === 'development') {
   console.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
