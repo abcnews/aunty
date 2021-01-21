becc5ea6d7a1e62f04b02eba23235679ff12f7d1
Colin Gourlay <gourlay.colin@abc.net.au>
2021-01-21 10:35:43 +1000

    Update react project template in the same fashion as the basic project template

--- src/generators/project/templates/react/src/index.tsx
+++ src/generators/project/templates/react/src/index.tsx
@@ -1,28 +1,40 @@
+import * as acto from '@abcnews/alternating-case-to-object';
+import { <% if (isOdyssey) { %>whenOdysseyLoaded<% } else { %>whenDOMReady<% } %> } from '@abcnews/env-utils';
+import { getMountValue, selectMounts } from '@abcnews/mount-utils';<% if (isTS) { %>
+import type { Mount } from '@abcnews/mount-utils';<% } %>
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import App from './components/App';<% if (isTS) { %>
+import type { AppProps } from './components/App';<% } %>
 
-const PROJECT_NAME<% if (isTS) { %>: string<% } %> = '<%= projectSlug %>';
-const root = document.querySelector(`[data-${PROJECT_NAME}-root]`);
+let appMountEl<% if (isTS) { %>: Mount<% } %>;
+let appProps<% if (isTS) { %>: AppProps<% } %>;
 
-function init() {
-  render(<App projectName={PROJECT_NAME} />, root);
+function renderApp() {
+  render(<App {...appProps} />, appMountEl);
 }
 
-init();
+<% if (isOdyssey) { %>whenOdysseyLoaded<% } else { %>whenDOMReady<% } %>.then(() => {
+  [appMountEl] = selectMounts('<%= projectNameFlat %>');
+
+  if (appMountEl) {
+    appProps = acto(getMountValue(appMountEl));
+    renderApp();
+  }
+});
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
     try {
-      init();
+      renderApp();
     } catch (err) {
       import('./components/ErrorBox').then(({ default: ErrorBox }) => {
-        render(<ErrorBox error={err} />, root);
+        render(<ErrorBox error={err} />, appMountEl);
       });
     }
   });
 }
 
 if (process.env.NODE_ENV === 'development') {
-  console.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
+  console.debug(`[<%= projectName %>] public path: ${__webpack_public_path__}`);
 }
