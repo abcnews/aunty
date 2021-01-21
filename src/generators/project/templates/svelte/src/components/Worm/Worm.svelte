dafa815106b072f10c00606fb7959f449b70ddf1
Colin Gourlay <gourlay.colin@abc.net.au>
2021-01-21 10:18:56 +1000

    Update Vue and Svelte Worm components to use their latest recommended single-file component section ordering

--- src/generators/project/templates/svelte/src/components/Worm/Worm.svelte
+++ src/generators/project/templates/svelte/src/components/Worm/Worm.svelte
@@ -2,11 +2,11 @@
   import worm from './worm.svg';
 </script>
 
+<img src={worm} alt="" />
+
 <style lang="scss">
   img {
     width: 240px;
     height: auto;
   }
 </style>
-
-<img src={worm} alt="" />
