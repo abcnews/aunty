43fa36b1728af0e0f714874d0df04e36898682dc
Colin Gourlay <gourlay.colin@abc.net.au>
2021-04-15 14:29:26 +1000

    Add includedDependencies prop to build config which can be used to list packages in node_modules (as strings or regex patterns) that we want babel to transpile.

--- src/config/webpack.js
+++ src/config/webpack.js
@@ -115,9 +115,27 @@ module.exports.getWebpackConfig = () => {
   return config;
 };
 
+const resolveIncludedDependencies = (includedDependencies, root) => {
+  if (!Array.isArray(includedDependencies)) {
+    return [];
+  }
+
+  return includedDependencies.map(packageNameOrPattern => {
+    if (typeof packageNameOrPattern === 'string') {
+      return resolve(root, 'node_modules', packageNameOrPattern);
+    }
+
+    if (packageNameOrPattern instanceof RegExp) {
+      return new RegExp(join('node_modules', packageNameOrPattern.source), packageNameOrPattern.flags);
+    }
+
+    return null;
+  });
+};
+
 function createWebpackConfig({ isModernJS } = {}) {
   const { pkg, root, hasTS, type, webpack: projectWebpackConfig } = getProjectConfig();
-  const { entry, extractCSS, from, staticDir, to, useCSSModules } = getBuildConfig();
+  const { entry, extractCSS, from, includedDependencies, staticDir, to, useCSSModules } = getBuildConfig();
   const isProd = process.env.NODE_ENV === 'production';
 
   const config = merge(
@@ -147,7 +165,7 @@ function createWebpackConfig({ isModernJS } = {}) {
           {
             __hint__: 'scripts',
             test: hasTS ? /\.m?[jt]sx?$/ : /\.m?jsx?$/,
-            include: [resolve(root, from)],
+            include: [resolve(root, from)].concat(resolveIncludedDependencies(includedDependencies, root)),
             loader: require.resolve('babel-loader'),
             options: getBabelConfig({ isModernJS })
           },
