ddd8016d106c3dc60cef5f2f0d57a791a7ff77ee
Simon Elvery <simon@elvery.net>
2023-07-20 09:26:45 +1000

    Define `resolve.conditionNames` in Svelte Webpack config

--- src/config/webpack.js
+++ src/config/webpack.js
@@ -44,7 +44,8 @@ const PROJECT_TYPES_CONFIG = {
       extensions: [...config.resolve.extensions, '.svelte'],
       // When using Svelte components installed from npm, use the original component
       // source code, rather than consuming the already-compiled version
-      mainFields: ['svelte', 'browser', 'module', 'main']
+      mainFields: ['svelte', 'browser', 'module', 'main'],
+      conditionNames: ['svelte', 'browser', 'import']
     };
 
     const { include, loader, options } = getHintedRule(config, 'scripts');
