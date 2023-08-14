da54a435e2744887735bd0f5775da3364492be84
Ash Kyd <kyd.ashley@abc.net.au>
2023-08-14 14:44:31 +1000

    fix type error in Preact projects

--- src/generators/project/templates/preact/src/index.tsx
+++ src/generators/project/templates/preact/src/index.tsx
@@ -26,7 +26,7 @@ if (module.hot) {
   module.hot.accept('./components/App', () => {
     try {
       renderApp();
-    } catch (err) {
+    } catch (err: any) {
       import('./components/ErrorBox').then(({ default: ErrorBox }) => {
         if (appMountEl) {
           render(<ErrorBox error={err} />, appMountEl);
