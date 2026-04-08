0a824133e7f11efea6d686815c2e322872a7c019
Ash Kyd <kyd.ashley@abc.net.au>
2026-04-08 14:23:52 +1000

    bug: dead code

--- src/commands/create/types.ts
+++ src/commands/create/types.ts
@@ -5,19 +5,3 @@ export interface InitOptions {
   projectName: string;
   baseDir: string;
 }
-
-/**
- * A patch that can be applied to a template.
- */
-export interface Patch {
-  name: string;
-  init: (options: InitOptions) => Promise<void>;
-}
-
-/**
- * A question asked during project creation to determine if a patch should be applied.
- */
-export interface Question {
-  question: string;
-  action: string;
-}
